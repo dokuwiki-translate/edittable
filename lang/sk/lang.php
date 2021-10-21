4cb5f474fecae781e9188a26f61cd561afe0655c
Shankar Rao <shankar.rao@gmail.com>
2021-10-21 10:05:04 +0200

    translation update

---
+++ lang/sk/lang.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Shankar Rao <shankar.rao@gmail.com>
+ */
+$lang['secedit_name']          = 'Tabuľka';
+$lang['add_table']             = 'Vložiť novú tabuľku ';
+$lang['js']['toggle_header']   = 'Prepnúť stav hlavičky ';
+$lang['js']['align_left']      = 'Zarovnať bunku doľava ';
+$lang['js']['align_center']    = 'Zarovnať bunku v strede ';
+$lang['js']['align_right']     = 'Zarovnať bunku doprava';
+$lang['js']['confirmdeleterow'] = 'Skutočne zmazať riadok?';
+$lang['js']['confirmdeletecol'] = 'Skutočne zmazať stĺpec? ';
+$lang['js']['row_above']       = 'Pridať riadok vyššie ';
+$lang['js']['remove_row']      = 'Odstrániť riadok ';
+$lang['js']['row_below']       = 'Pridať riadok nižšie';
+$lang['js']['col_left']        = 'Pridať stĺpec doľava';
+$lang['js']['remove_col']      = 'Odstrániť stĺpec';
+$lang['js']['col_right']       = 'Pridať stĺpec doprava';
+$lang['js']['merge_cells']     = 'Spojiť bunky';
+$lang['js']['unmerge_cells']   = 'Rozdeliť bunky';
