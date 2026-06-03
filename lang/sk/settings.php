d4fa4fd13415f25989e21dc10b6809bbf7eca7bb
Wizzard <wizzardsk@gmail.com>
2026-06-03 20:30:53 +0200

    Translation update (sk)

---
+++ lang/sk/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
+ */
+$lang['default colwidth']      = 'Šírka stĺpcov tabuľky. Ponechajte prázdne, aby sa šírka odvodila od obsahu.';
